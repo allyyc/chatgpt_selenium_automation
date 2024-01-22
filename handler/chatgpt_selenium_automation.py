7582e06c78e7ef81846a4794e63c1ecc537d842d
allyyc <allisonscasasola@gmail.com>
2024-01-22 11:43:42 -0800

    modified main.py and chatgpt_selenium_automation.py

--- handler/chatgpt_selenium_automation.py
+++ handler/chatgpt_selenium_automation.py
@@ -12,8 +12,9 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 
-
-
+import random
+import csv
+import sys
 
 class ChatGPTAutomation:
 
@@ -30,15 +31,108 @@ class ChatGPTAutomation:
 
         self.chrome_path = chrome_path
         self.chrome_driver_path = chrome_driver_path
-
-        # url = r"https://chat.openai.com"
-        url = r"https://chat.openai.com/g/g-pGibIwKEi-gpt-4-rag"
-        free_port = self.find_available_port()
-        self.launch_chrome_with_remote_debugging(free_port, url)
+        self.path_to_csv = "questions.csv"
+        self.url = r"https://chat.openai.com/g/g-cy3cGFjNo-gpt-4-rag"
+        self.free_port = self.find_available_port()
+        self.launch_chrome_with_remote_debugging(self.free_port, self.url)
         self.wait_for_human_verification()
-        self.driver = self.setup_webdriver(free_port)
+        self.driver = self.setup_webdriver(self.free_port)
+        self.input_box = self.driver.find_element(By.ID, "prompt-textarea")
+        self.data = self.read_csv(self.path_to_csv)
 
+    def setup_driver(self):
+        self.free_port = self.find_available_port()
+        self.launch_chrome_with_remote_debugging(self.free_port, self.url)
+        self.driver = self.setup_webdriver(self.free_port)
 
+    def read_csv(self, path_to_csv):
+        """ Reads content of .csv into list of lists """
+        with open(path_to_csv, newline='') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            next(csv_reader, None)
+            data = list(csv_reader)
+        return data
+    
+    def write_csv(self, path_to_csv):
+        """ Writes content of self.data to .csv file """
+        with open(path_to_csv, 'w', newline='') as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerows(self.data)
+
+    def is_csv_full(self):
+        """ Checks if .csv file has responses & citations filled in for every prompt """
+        for row in self.data:
+            if row[4] == 'GPT 4 RAG' or not row[4].strip() or row[5] == '[]':
+                return False
+        return True
+
+    def loop_through_prompts(self):
+        try:
+            # TO-DO: count, .csv_file as a hyperparameter
+            full_csv = self.is_csv_full()
+            count = 0
+            while full_csv == False:
+                for row in self.data:
+
+                    # Checks if current row does not have a response or citation
+                    if len(row) > 4 and (row[4] == 'GPT 4 RAG' or not row[4].strip() or row[5] == '[]') and count < 7:
+                        count += 1
+                        cur_prompt = row[3]
+                        
+                        # Processes str of prompt and sends to GPT
+                        print("Entering prompt to GPT: ", cur_prompt)
+                        cur_prompt = cur_prompt.replace("'", "\\'")
+                        self.send_prompt_to_chatgpt(str(cur_prompt))
+                        time.sleep(30)
+
+                        # TO-DO: Create time-out (max of 150 seconds before opening new browser?)
+
+                        # Lets response load until "Stop generating" button disappears
+                        while self.is_button_present("//button[@aria-label='Stop generating']"):
+                            time.sleep(20)
+
+                        time.sleep(30)
+
+                        # If GPT-4 raises an error, writes current conversation into .csv file and opens new chat
+                        if "There was an error generating a response" in self.driver.page_source or "network error" in self.driver.page_source:
+                            print("Loading error. Will read current conversation into csv, then generate new page")
+                            time.sleep(45)
+                            conversation = self.return_chatgpt_conversation()
+                            self.process_conversation(conversation)
+                            print("Read items into csv. Loading new page...")
+                            link = self.driver.find_element(By.CSS_SELECTOR, "a[href='/g/g-cy3cGFjNo-gpt-4-rag']")
+                            link.click()
+                            time.sleep(60)
+                            self.input_box = self.driver.find_element(By.ID, "prompt-textarea")
+                        else:
+                            print("No errors, next response")
+                    elif count == 7:
+                        print("Max count reached")
+                        time.sleep(45)
+                        conversation = self.return_chatgpt_conversation()
+                        self.process_conversation(conversation)
+                        print("Read items into csv. Loading new page...")
+                        link = self.driver.find_element(By.CSS_SELECTOR, "a[href='/g/g-cy3cGFjNo-gpt-4-rag']")
+                        link.click()
+                        time.sleep(60)
+                        self.input_box = self.driver.find_element(By.ID, "prompt-textarea")
+                        count = 0
+                        
+
+                # Once all queries are made, fills out .csv with data
+                print("Filling in .csv")
+                conversation = self.return_chatgpt_conversation()
+                self.process_conversation(conversation)
+                full_csv = self.is_csv_full()
+            self.end_session()
+        
+        # Opens new chat in case of any miscellaneous behavior
+        except:
+            print("Except condition")
+            conversation = self.return_chatgpt_conversation()
+            self.process_conversation(conversation)
+            full_csv = self.is_csv_full()
+            self.end_session()  
 
     def find_available_port(self):
         """ This function finds and returns an available port number on the local machine by creating a temporary
@@ -48,8 +142,7 @@ class ChatGPTAutomation:
             s.bind(('', 0))
             s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             return s.getsockname()[1]
-
-
+        
 
     def launch_chrome_with_remote_debugging(self, port, url):
         """ Launches a new Chrome instance with remote debugging enabled on the specified port and navigates to the
@@ -63,7 +156,6 @@ class ChatGPTAutomation:
         chrome_thread.start()
 
 
-
     def setup_webdriver(self, port):
         """  Initializes a Selenium WebDriver instance, connected to an existing Chrome browser
              with remote debugging enabled on the specified port"""
@@ -74,30 +166,69 @@ class ChatGPTAutomation:
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
                                   options=chrome_options)
         return driver
+    
 
-
+    def is_button_present(self, xpath):
+        try:
+            self.driver.find_element(By.XPATH, xpath)
+            return True
+        except:
+            return False
 
     def send_prompt_to_chatgpt(self, prompt):
         """ Sends a message to ChatGPT and waits for 20 seconds for the response """
+        try:
+            self.input_box.click()
+            self.driver.execute_script(f"arguments[0].value = '{prompt}';", self.input_box)
+            self.input_box.send_keys(Keys.RETURN)
+            self.input_box.submit()
+        except:
+            print("Error loading screen.. Will open new one")
+            time.sleep(45)
+            conversation = self.return_chatgpt_conversation()
+            self.process_conversation(conversation)
+            print("Read items into csv. Loading new page...")
 
-        # input_box = self.driver.find_element(by=By.XPATH, value='//textarea[contains(@placeholder, "Message ChatGPT...")]')
-        input_box = self.driver.find_element(By.ID, "prompt-textarea")
-        self.driver.execute_script(f"arguments[0].value = '{prompt}';", input_box)
-        input_box.send_keys(Keys.RETURN)
-        input_box.submit()
-        # print("---SLEEPING---")
-        # time.sleep(100)
+            link = self.driver.find_element(By.CSS_SELECTOR, "a[href='/g/g-cy3cGFjNo-gpt-4-rag']")
+            link.click()
 
+            time.sleep(90)
+            self.input_box = self.driver.find_element(By.ID, "prompt-textarea")
+            self.send_prompt_to_chatgpt(prompt)
 
 
     def return_chatgpt_conversation(self):
         """
-        :return: returns a list of items, even items are the submitted questions (prompts) and odd items are chatgpt response
+        :return: returns a list of items, every even pair are the submitted questions (prompts) and odd pair of items are chatgpt response
+        Ex: [question1, question1, response1, response1, question2, question2, response2, response2]
         """
+        conversation = self.driver.find_elements(by=By.CSS_SELECTOR, value='div.text-base')
+        for item in conversation:
+            print(item.text)
+        return conversation
+    
 
-        return self.driver.find_elements(by=By.CSS_SELECTOR, value='div.text-base')
+    def process_conversation(self, conversation):
+        """
+        Writes responses and URLs from conversation to .csv ifile
+        """
+        for i in range(0, len(conversation), 4):  
+            prompt = conversation[i].text
+            for sublist in self.data:
+                if len(sublist) >= 4 and sublist[3] in prompt:
+                    print("Entering data into .csv row")
+                    print("Question:", prompt)
+                    response_element = conversation[i+2]
+                    print("Response:", response_element.text)
+                    sublist[4] = response_element.text
+                    print(sublist[4])
+                    print("URLs:")
+                    urls = self.return_url_citations(response_element)
+                    print(urls)
+                    sublist[5] = urls
 
-
+        self.write_csv(self.path_to_csv)
+ 
 
     def save_conversation(self, file_name):
         """
@@ -123,32 +254,28 @@ class ChatGPTAutomation:
                     f"prompt: {chatgpt_conversation[i].text}\nresponse: {chatgpt_conversation[i + 1].text}\n\n{delimiter}\n\n")
 
 
-
     def return_last_response(self):
         """ :return: the text of the last chatgpt response """
 
         response_elements = self.driver.find_elements(by=By.CSS_SELECTOR, value='div.text-base')
-        html_content = response_elements[-1].get_attribute("outerHTML")
-        print(html_content)
-        print("---SLEEPING---")
-        # time.sleep(20)
+        return response_elements[-1]
+    
+    
+    def return_url_citations(self, response_element):
+        """Retrieves URLs from response"""
+        html_content = response_element.get_attribute("outerHTML")
         soup = BeautifulSoup(html_content, 'html.parser')
         a_tags = soup.find_all('a')
         url_links = [a.get('href') for a in a_tags if a.has_attr('href')]
-        print("-------URL LINKS-------")
-        print(url_links)
-        # print("---SLEEPING---")
-        # time.sleep(100)
-        return response_elements[-1].text
-
-
+        return(url_links)
+    
 
     def wait_for_human_verification(self):
         print("You need to manually complete the log-in or the human verification if required.")
 
         while True:
             user_input = input(
-                "Enter 'y' if you have completed the log-in or the human verification, or 'n' to check again: ").lower()
+                "Enter 'y' if you have completed the log-in or the human verification, or 'n' to check again. Make sure page is fully loaded before entering: ").lower()
 
             if user_input == 'y':
                 print("Continuing with the automation process...")
@@ -160,11 +287,23 @@ class ChatGPTAutomation:
                 print("Invalid input. Please enter 'y' or 'n'.")
 
 
+    def end_session(self):
+        while True:
+            user_input = input("Enter e if you want to end this session").lower()
+            if user_input == 'e':
+                print("Saving current data, ending session")
+                time.sleep(10)
+                conversation = self.return_chatgpt_conversation()
+                self.process_conversation(conversation)
+                self.quit()
+            sys.exit(0)
 
     def quit(self):
         """ Closes the browser and terminates the WebDriver session."""
         print("Closing the browser...")
         self.driver.close()
         self.driver.quit()
+
+
 
 
--- main.py
+++ main.py
@@ -3,6 +3,30 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from handler.chatgpt_selenium_automation import ChatGPTAutomation
 import time
+import csv
+import threading
+
+
+
+def get_prompts_from_csv(path):
+    import csv
+
+    prompts = []
+
+    # Open the CSV file and read it
+    with open(path, newline='') as csvfile:
+
+        csv_reader = csv.reader(csvfile)
+
+        # Skip the header if your CSV has one
+        next(csv_reader)
+
+        # Iterate over the rows in the CSV
+        for row in csv_reader:
+            if row:  # Check if row is not empty
+                # Assuming question is in fourth column
+                prompts.append(row[3])  # Append the second column data
+    return prompts
 
 options = Options()
 options.add_experimental_option("detach", True)
@@ -10,23 +34,19 @@ options.add_experimental_option("detach", True)
 #                          options=options)
 chrome_driver_path = r"/Users/allisoncasasola/medical-ai/chromedriver-mac-arm64/chromedriver"
 chrome_path = r'"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"'
+csv_path = "data/test.csv"
+# prompts = get_prompts_from_csv(csv_path)
 chatgpt = ChatGPTAutomation(chrome_path, chrome_driver_path)
 
-questions = ["What are the benefits of exercise?", "What happens to someone who lays in bed 24/7?", "What are the benefits of eating well?"]
+chatgpt.loop_through_prompts()
 
-# for question in questions:
-#     prompt = question
-#     chatgpt.send_prompt_to_chatgpt(prompt)
-#     response = chatgpt.return_last_response()
-#     print(response)
+# thread1 = threading.Thread(target=chatgpt.loop_through_prompts())
+# thread2 = threading.Thread(target=chatgpt.end_session())
 
-# Define a prompt and send it to chatGPT
-prompt = "What are the benefits of exercise?"
-chatgpt.send_prompt_to_chatgpt(prompt)
+# Start threads
+# thread2.start()
+# thread1.start()
 
-user_input = input(
-    "Enter 'y' to retrieve response: ").lower()
-
-# Retrieve the last response from chatGPT
-response = chatgpt.return_last_response()
-print(response)
+# Join threads to wait for them to finish (optional, if you want the main program to wait for these threads)
+# thread1.join()
+# thread2.join()
